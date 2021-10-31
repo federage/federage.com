38df6af86d7c23e8704ece9f868ce1b9bc551f2c
Mathieu Darse <code@mathieudarse.fr>
2021-10-31 23:40:10 +0000

    Add some styling

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,32 @@
-<h1>Federage</h1>
-<h2>Produire, financer et gérer son portefeuille d’actifs</h2>
-<p>
-	Notre groupement d’intérêt économique (GIE) permet aux professionnels de développer leurs
-	capacités d’auto-financement et de piloter leurs opérations en lien avec leurs partenaires
-	économiques.
-</p>
-<p>
-	Toute entreprise peut co-produire des actifs au sein d’une communauté professionnelle, détenir une
-	prise de participation du capital et bénéficier de la valeur ajoutée de cette communauté.
-</p>
-<p>
-	En tant que commun numérique, nous avons rendu le développement logiciel aussi ouvert que possible
-	et nous vous encourageons à développer puis partager vos contributions.
-</p>
+<div class="max-w-4xl mx-auto">
+    <div class="sm:m-8 p-6 bg-white sm:rounded-2xl shadow-xl">
+        <h1 class="mt-4 mb-6 text-6xl font-mono font-bold lowercase">Federage</h1>
+        <h2 class="my-8 text-3xl text-gray-800">Produire, financer et gérer son portefeuille d’actifs</h2>
+        <div class="text-2xl text-gray-700 leading-relaxed space-y-4">
+            <p>
+                Notre groupement d’intérêt économique (GIE) permet aux professionnels de développer leurs
+                capacités d’auto-financement et de piloter leurs opérations en lien avec leurs partenaires
+                économiques.
+            </p>
+            <p>
+                Toute entreprise peut co-produire des actifs au sein d’une communauté professionnelle, détenir
+                une prise de participation du capital et bénéficier de la valeur ajoutée de cette communauté.
+            </p>
+            <p>
+                En tant que commun numérique, nous avons rendu le développement logiciel aussi ouvert que
+                possible et nous vous encourageons à développer puis partager vos contributions.
+            </p>
+        </div>
+    </div>
+</div>
+
+<footer class="max-w-4xl mx-auto mt-20 p-6">
+	<div class="text-3xl text-gray-800">federage, groupement d’intérêt économique</div>
+	<div class="text-gray-400">Dernier commit : e9bde668d3j38f49200555d78a8b239x19845320</div>
+</footer>
+
+<style>
+	:global(body) {
+		@apply bg-[#F9FAFA];
+	}
+</style>
