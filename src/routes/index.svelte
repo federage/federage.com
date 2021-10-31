fbada81f8f43f0baed71569f5f3a03f538e3fb0a
Mathieu Darse <code@mathieudarse.fr>
2021-10-31 20:39:38 +0000

    Add home page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Federage</h1>
+<h2>Produire, financer et gérer son portefeuille d’actifs</h2>
+<p>
+	Notre groupement d’intérêt économique (GIE) permet aux professionnels de développer leurs
+	capacités d’auto-financement et de piloter leurs opérations en lien avec leurs partenaires
+	économiques.
+</p>
+<p>
+	Toute entreprise peut co-produire des actifs au sein d’une communauté professionnelle, détenir une
+	prise de participation du capital et bénéficier de la valeur ajoutée de cette communauté.
+</p>
+<p>
+	En tant que commun numérique, nous avons rendu le développement logiciel aussi ouvert que possible
+	et nous vous encourageons à développer puis partager vos contributions.
+</p>
