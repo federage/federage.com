1800f5e501310534d49b1f31479b9d83d0403a05
Mathieu Darse <code@mathieudarse.fr>
2021-11-01 23:17:56 +0000

    Deploy to Netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,6 +8,10 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
+		adapter: adapter(),
+		appDir: '.',
+		hydrate: false,
+		router: false,
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
